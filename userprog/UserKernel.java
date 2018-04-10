fe0f2901e6e2d02bab57107d4c54a11a0d1637b9
Erumya <windee2@hotmail.com>
2018-04-10 19:40:49 -0400

    Fixed some code and a bug

--- userprog/UserKernel.java
+++ userprog/UserKernel.java
@@ -1,19 +1,27 @@
 package nachos.userprog;
 
+import java.util.LinkedList;
 import nachos.machine.*;
 import nachos.threads.*;
-
-import java.util.LinkedList;
+import nachos.userprog.*;
 
 /**
  * A kernel that can support multiple user processes.
  */
 public class UserKernel extends ThreadedKernel {
+	
+		// Data fields
+	private static Lock pageLock = new Lock();
+	int offsetLength;
+	static LinkedList<Integer> memoryPage = new LinkedList<Integer>();
+	static LinkedList<Integer> frameTable;
+	static LinkedList<Integer> availablePages = new LinkedList<Integer>();
+	
     /**
      * Allocate a new user kernel.
      */
     public UserKernel() {
-        super();
+	super();
     }
 
     /**
@@ -21,66 +29,57 @@ public class UserKernel extends ThreadedKernel {
      * processor's exception handler.
      */
     public void initialize(String[] args) {
-        super.initialize(args);
+	super.initialize(args);
 
-        console = new SynchConsole(Machine.console());
-
-        Machine.processor().setExceptionHandler(new Runnable() {
-            public void run() {
-                exceptionHandler();
-            }
-        });
-
-        offsetLen = 0;
-        for (offsetLen = 0; ; ++offsetLen)
-            if ((Processor.pageSize >> offsetLen) == 1) {
-                offsetMask = (1 << offsetLen) - 1;
-                break;
-            }
-
-        pageLock = new Lock();
-
-        int numPhysPages = Machine.processor().getNumPhysPages();
-        availablePages = new LinkedList<Integer>();
-        for (int i = 0; i < numPhysPages; ++i)
-            availablePages.add(new Integer(i));
+	console = new SynchConsole(Machine.console());
+	
+	Machine.processor().setExceptionHandler(new Runnable() {
+		public void run() { exceptionHandler(); } });
+   
+	int numPPages = Machine.processor().getNumPhysPages();
+	frameTable = new LinkedList<Integer>(); 
+	for(int i = 0; i < numPPages; i++){
+		frameTable.add(new Integer(i));
+		}
     }
+
 
     /**
      * Test the console device.
-     */
+     */	
     public void selfTest() {
-        super.selfTest();
+	super.selfTest();
 
-        System.out.println("Testing the console device. Typed characters");
-        System.out.println("will be echoed until q is typed.");
+	System.out.println("Testing the console device. Typed characters");
+	System.out.println("will be echoed until q is typed.");
 
-        char c;
+	char c;
 
-        do {
-            c = (char) console.readByte(true);
-            console.writeByte(c);
-        } while (c != 'q');
+	do {
+	    c = (char) console.readByte(true);
+	    console.writeByte(c);
+	}
+	while (c != 'q');
 
-        System.out.println("");
+	System.out.println("");
     }
 
     /**
      * Returns the current process.
-     * 
-     * @return the current process, or <tt>null</tt> if no process is current.
+     *
+     * @return	the current process, or <tt>null</tt> if no process is current.
      */
     public static UserProcess currentProcess() {
-        if (!(KThread.currentThread() instanceof UThread))
-            return null;
-
-        return ((UThread) KThread.currentThread()).process;
+	if (!(KThread.currentThread() instanceof UThread))
+	    return null;
+	
+	return ((UThread) KThread.currentThread()).process;
     }
 
     /**
-     * The exception handler. This handler is called by the processor whenever a
-     * user instruction causes a processor exception.
-     * 
+     * The exception handler. This handler is called by the processor whenever
+     * a user instruction causes a processor exception.
+     *
      * <p>
      * When the exception handler is invoked, interrupts are enabled, and the
      * processor's cause register contains an integer identifying the cause of
@@ -91,84 +90,88 @@ public class UserKernel extends ThreadedKernel {
      * that caused the exception.
      */
     public void exceptionHandler() {
-        Lib.assertTrue(KThread.currentThread() instanceof UThread);
+	Lib.assertTrue(KThread.currentThread() instanceof UThread);
 
-        UserProcess process = ((UThread) KThread.currentThread()).process;
-        int cause = Machine.processor().readRegister(Processor.regCause);
-        process.handleException(cause);
+	UserProcess process = ((UThread) KThread.currentThread()).process;
+	int cause = Machine.processor().readRegister(Processor.regCause);
+	process.handleException(cause);
     }
 
     /**
      * Start running user programs, by creating a process and running a shell
      * program in it. The name of the shell program it must run is returned by
      * <tt>Machine.getShellProgramName()</tt>.
-     * 
-     * @see nachos.machine.Machine#getShellProgramName
+     *
+     * @see	nachos.machine.Machine#getShellProgramName
      */
     public void run() {
-        super.run();
+	super.run();
 
-        UserProcess process = UserProcess.newUserProcess();
-        root = process;
+	UserProcess process = UserProcess.newUserProcess();
+	
+	root = process;
+	String shellProgram = Machine.getShellProgramName();	
+	Lib.assertTrue(process.execute(shellProgram, new String[] { }));
 
-        String shellProgram = Machine.getShellProgramName();
-        Lib.assertTrue(process.execute(shellProgram, new String[] {}));
-
-        KThread.finish();
+	KThread.currentThread().finish();
     }
-
-    protected UserProcess root;
 
     /**
      * Terminate this kernel. Never returns.
      */
     public void terminate() {
-        super.terminate();
+	super.terminate();
     }
 
     public static int getVirtualPageNumber(int vaddr) {
         return Machine.processor().pageFromAddress(vaddr);
-    }
-
-    public static int getOffset(int vaddr) {
-        return vaddr & offsetMask;
-    }
-
-    public static int makeAddress(int pn, int offset) {
-        return Machine.processor().makeAddress(pn, offset);
-    }
-
+ }
+    
+    /**
+     * Will add a new Page
+     * @return 	The new page.
+     */
     public static int newPage() {
-        int ret = -1;
-
-        pageLock.acquire();
-        if (availablePages.size() > 0)
-            ret = availablePages.removeFirst().intValue();
-        pageLock.release();
-
-        return ret;
+    	int newPage=-1;
+    	
+    	pageLock.acquire();
+    	
+    	if(frameTable.size()>0) {
+    		newPage=frameTable.removeFirst().intValue();
+    	}
+    	pageLock.release();
+    
+    		return newPage;
     }
 
+    
+    /**
+     * This method will delete a requested Page with the PPN.
+     * @param ppn	Physical Page Number variable that is currently
+     * 				wanting to be deleted
+     * @return		true that the page is deleted if existent. If not false.
+     */
     public static boolean deletePage(int ppn) {
-        boolean ret = false;
+    	boolean deleted=false;
 
-        pageLock.acquire();
-        availablePages.add(new Integer(ppn));
-        ret = true;
-        pageLock.release();
-
-        return ret;
-    }
-
-    public String absoluteFileName(String s) {
-        return s;
-    }
-
+    	pageLock.acquire();
+    	
+    	if(ppn >= 0 && ppn < Machine.processor().getNumPhysPages())
+    	{
+    		Integer pageNum = new Integer(ppn);
+    		frameTable.add(new pageNum);
+    		deleted=true;
+    	}
+    	pageLock.release();
+    	
+    	return deleted;
+    }    
+    
     /** Globally accessible reference to the synchronized console. */
     public static SynchConsole console;
 
-    private static int offsetLen, offsetMask;
-
-    private static Lock pageLock;
-    private static LinkedList<Integer> availablePages;
+    // dummy variables to make javac smarter
+    private static Coff dummy1 = null;
+    
+    public static UserProcess root = null;
 }
